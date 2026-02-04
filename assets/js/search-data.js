b61e2ea035cf5e373fb60f62ae49ba4bbffdcb11
meg-briers <129373355+meg-briers@users.noreply.github.com>
2026-02-04 19:20:30 +0000

    Deploying to gh-pages from @ meg-briers/meg-briers.github.io@1a00e6c0af04c179c3b97d2f8314f26392bda8ac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,16 +426,13 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-a-whole-lot-of-new-websites",
+          title: 'a whole lot of new websites',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-tickets-are-released-for-my-public-talk-in-march-see-brewing-minds-for-more",
+          title: 'Tickets are released for my public talk in March, see Brewing Minds for...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
